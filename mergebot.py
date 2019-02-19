eb38e313d87ce9925c2baa2385017bdc88b5cc75
Diego Essaya <dessaya@gmail.com>
2019-02-19 10:58:36 -0300

    show required checks

--- mergebot.py
+++ mergebot.py
@@ -10,6 +10,8 @@ import re
 import platform
 import argparse
 
+DEBUG = False
+
 parser = argparse.ArgumentParser(
     description='Periodically poll a Github pull request and merge it automatically if all checks pass.'
 )
@@ -78,7 +80,7 @@ except KeyError:
 
 def request(path, data=None, method='GET'):
     url = f"https://{DOMAIN}/api/v3{path}"
-    debug(f"{method} {url}")
+    if DEBUG: debug(f"{method} {url}")
     response = urlopen(Request(
         url,
         headers={
@@ -93,8 +95,27 @@ def request(path, data=None, method='GET'):
 def pull_get(path):
     return request(f"/repos/{REPO}/pulls/{PR}{path}")
 
+def required_statuses_get(branch):
+    return set(request(f"/repos/{REPO}/branches/{branch}")['protection']['required_status_checks']['contexts'])
+
 def statuses_get(sha):
-    return request(f"/repos/{REPO}/statuses/{sha}")
+    required_statuses = required_statuses_get(pr['base']['ref'])
+    statuses = request(f"/repos/{REPO}/statuses/{sha}")
+    # Add required flag
+    statuses = [{**s, 'required': s['context'] in required_statuses} for s in statuses]
+    # Keep the latest status for each context
+    statuses = [list(v)[-1] for k, v in itertools.groupby(
+        sorted(statuses, key=lambda s: (s['context'], s['updated_at'])),
+        lambda s: s['context'],
+    )]
+    # group by state (success, pending, failure, ...)
+    return dict(
+        (k, list(v))
+        for k, v in itertools.groupby(
+            sorted(statuses, key=lambda s: s['state']),
+            lambda s: s['state'],
+        )
+    )
 
 def merge(commit_title, sha):
     return request(
@@ -107,6 +128,12 @@ def merge(commit_title, sha):
         }).encode('utf8'),
         method='PUT'
     )
+
+def list_checks(checks, show, message, display_required):
+    if len(checks) > 0:
+        show(message, display_required and any(c['required'] for c in checks))
+        for c in checks:
+            show(f"    {c['context']} {'(Required) ' if c['required'] else ''}({c['target_url'] or c['description']})")
 
 user = request('/user')['login']
 info(f'Logged in as {user}')
@@ -138,31 +165,8 @@ while True:
         break
 
     statuses = statuses_get(sha)
-    # Keep the latest status for each context
-    statuses = [list(v)[-1] for k, v in itertools.groupby(
-        sorted(statuses, key=lambda s: (s['context'], s['updated_at'])),
-        lambda s: s['context'],
-    )]
-    # group by state (success, pending, failure, ...)
-    grouped_statuses = dict(
-        (k, list(v))
-        for k, v in itertools.groupby(
-            sorted(statuses, key=lambda s: s['state']),
-            lambda s: s['state'],
-        )
-    )
-
-    failures = grouped_statuses.get('error', []) + grouped_statuses.get('failure', [])
-    if len(failures) > 0:
-        error("Some checks were not successful", True)
-        for check in failures:
-            error(f"    {check['context']} ({check['target_url'] or check['description']})")
-
-    pending = grouped_statuses.get('pending', [])
-    if len(pending) > 0:
-        warn("Pending checks:")
-        for check in pending:
-            warn(f"    {check['context']} ({check['target_url'] or check['description']})")
+    list_checks(statuses.get('error', []) + statuses.get('failure', []), error, 'Some checks were not successful:', True)
+    list_checks(statuses.get('pending', []), warn, 'Pending checks:', False)
 
     if mergeable_state == 'dirty':
         error('You have merge conflicts!', True)
